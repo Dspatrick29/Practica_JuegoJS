a8bf8a4112def2ce77a90337bccc6dce247bde6a
Dspatrick29 <150899742+Dspatrick29@users.noreply.github.com>
2024-11-15 12:13:42 +0100

    Implementacion boton inicio, intento ajuste gravedad

--- Practica_JuegoJS/juego.js
+++ Practica_JuegoJS/juego.js
@@ -1,9 +1,10 @@
 window.onload = function () {
-    let canvas, ctx;
+    let canvas;
     var suelo;
     var yArriba = false;
     var animacionActiva = true;
     canvas = document.getElementById("miCanvas");
+    const ctx = canvas.getContext('2d');
 
 
     // Constructor de Pajaro
@@ -11,69 +12,91 @@ window.onload = function () {
         this.r = 40;
         this.resetearVariables();
     }
-    //Metodo para inicializar la posicion del pajaro
+    
+    // Método para inicializar la posición del pájaro
     Pajaro.prototype.resetearVariables = function () {
         this.x = 250;
         this.y = 150;
-        this.velocidad = 0;
-        this.gravedad = 0.5;
-        this.salto = -10;
+        this.velocidad = 0;  // Velocidad inicial
+        this.gravedad = 0.5;  // Fuerza de gravedad
+        this.salto = -10;  // Fuerza del salto (valor negativo para que suba)
+        this.velocidadMaxima = 1;  // Velocidad máxima al caer
     };
-    //Metodo para dibujar el pajaro
+
+    // Método para dibujar el pájaro
     Pajaro.prototype.dibujar = function () {
         ctx.fillStyle = "orange";
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
         ctx.fill();
     };
-    //Metodo para actualizar la posicion del pajaro
+
+  
+    
+    // Método para dibujar el pájaro
+    Pajaro.prototype.dibujar = function () {
+        ctx.fillStyle = "orange";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        ctx.fill();
+    };
+
+    
+    // Método para actualizar la posición del pájaro
     Pajaro.prototype.actualizar = function () {
+        // Si el usuario presiona para saltar
         if (yArriba) {
-            pajaro.saltar();
+            this.velocidad = this.salto; // Reiniciar la velocidad al saltar
+            yArriba = false; // Desactivar el salto tras aplicarlo
         } else {
-            pajaro.y += 2.5;
+            // Aplicar gravedad solo si no hay salto
+            this.velocidad += this.gravedad;
+
+            // Limitar la velocidad máxima de caída
+            if (this.velocidad > this.velocidadMaxima) {
+                this.velocidad = this.velocidadMaxima;
+            }
         }
-         // Limitar al borde inferior (canvas.height - suelo.y - this.r)
-    // Limitar al borde inferior (canvas.height - suelo.y - this.r)
-    if (this.y + this.r >=  suelo.y) {
-        this.y =  suelo.y - this.r;
-        this.velY = 0; // Detener la velocidad de caída
-        animacionActiva = false;
-    console.log("colision");
-    }
-    };
-    Pajaro.prototype.saltar = function () {
-        this.y += this.salto;
+
+        // Actualizar la posición en función de la velocidad
+        this.y += this.velocidad;
+
+        // Limitar al borde inferior (suelo)
+        if (this.y + this.r >= suelo.y) {
+            this.y = suelo.y - this.r; // Ajustar la posición al borde
+            this.velocidad = 0; // Detener el movimiento
+            animacionActiva = false; // Detener la animación
+            console.log("Colisión con el suelo");
+        }
     };
 
 
+    // Crear el objeto su
     suelo = new Suelo();
     const pajaro = new Pajaro();
+
+    // Método para dibujar los elementos
     function dibujar() {
-
-
-        ctx = canvas.getContext("2d");
-        suelo.dibujar();
-        pajaro.dibujar();
-
+        ctx.clearRect(0, 0, canvas.width, canvas.height);  // Limpiar el canvas
+        suelo.dibujar();  // Dibujar el suelo
+        pajaro.dibujar();  // Dibujar el pájaro
     }
-    dibujar();
 
+    // Constructor del suelo
     function Suelo() {
         this.ancho = 1280;
         this.alto = 140;
         this.x = 0;
         this.y = 720 - this.alto;
         this.dibujar = function () {
-            ctx.fillStyle = "#F5F5DC"
-            ctx.fillRect(this.x, this.y, this.ancho, this.alto)
-        }
+            ctx.fillStyle = "#F5F5DC";
+            ctx.fillRect(this.x, this.y, this.ancho, this.alto);
+        };
     }
 
-
+    // Detectar eventos de teclado
     document.addEventListener("keydown", activaMovimiento, false);
-    document.addEventListener("keyup", desactivaMovimiento, false);
-
+   // document.addEventListener("keyup", desactivaMovimiento, false);
 
 
     function activaMovimiento(evt) {
@@ -86,7 +109,7 @@ window.onload = function () {
                 break;
         }
     }
-
+/*
     function desactivaMovimiento(evt) {
         switch (evt.keyCode) {
             case 38:
@@ -96,17 +119,22 @@ window.onload = function () {
                 yArriba = false;
                 break;
         }
-    }
-
-
-    setInterval(function () {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        dibujar();
+    }*/
+// Iniciar la animación
+document.getElementById('botonInicio').addEventListener('click', function () {
+    animacionActiva = true;
+    const intervalo = setInterval(function () {
         if (animacionActiva) {
-         
-        pajaro.actualizar();   
+            pajaro.actualizar(); // Actualizar la posición del pájaro
+            dibujar(); // Dibujar los elementos
+        } else {
+            clearInterval(intervalo); // Detener la animación al chocar con el suelo
         }
-    }, 1000 / 60);
+    }, 1000 / 60); // 60 FPS
+});
+
+
+
 
 
 
